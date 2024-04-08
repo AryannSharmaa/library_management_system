b3d7fa6422fa7376a736d684e17cb9d380f7313c
Aryan <aryancr72000@gmail.com>
2024-04-08 18:45:57 +0530

    implemented get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from models import Student, StudentUpdate
 from database import collection
+from schemas import list_serial, individual_serial
+from bson.objectid import ObjectId
 
 app = FastAPI()
 
@@ -13,3 +15,30 @@ async def create_stuents(student: Student):
         return {"id": str(id.inserted_id)}
     except:
         raise HTTPException(status_code=503, detail="error connecting to database")
+
+
+@app.get("/students")
+async def get_students(country: str | None = None, age: int | None = None):
+    students = list_serial(collection.find())
+    if country and age:
+        filtered = filter(
+            lambda x: x["address"]["country"] == country and x["age"] >= age, students
+        )
+        return {"students": list(filtered)}
+    if country:
+        filtered = filter(lambda x: x["address"]["country"] == country, students)
+        return {"students": list(filtered)}
+    if age:
+        filtered = filter(lambda x: x["age"] >= age, students)
+        return {"students": list(filtered)}
+    return {"students": (students)}
+
+
+@app.get("/students/{students_id}")
+async def get_student(student_id: str):
+    try:
+        student = individual_serial(collection.find_one({"_id": ObjectId(student_id)}))
+        print(student)
+        return {"student": dict(student)}
+    except:
+        raise HTTPException(status_code=404, detail="No student found")
--- schemas.py
+++ schemas.py
@@ -1,18 +1,11 @@
-def individual_serial(student, full=True):
-    if full:
-        return {
-            "id": str(student["_id"]),
-            "name": student["name"],
-            "age": student["age"],
-            "address": student["address"],
-        }
-    else:
-        return {
-            "id": str(student["_id"]),
-            "name": student["name"],
-            "age": student["age"],
-        }
+def individual_serial(student):
+    return {
+        "id": str(student["_id"]),
+        "name": student["name"],
+        "age": student["age"],
+        "address": student["address"],
+    }
 
 
 def list_serial(students):
-    return [individual_serial(student, full=False) for student in students]
+    return [individual_serial(student) for student in students]
